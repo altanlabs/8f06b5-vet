3a7740d98471ce801bd8e8bb88622ba9e57a343e
altanlabs <interfaces@altan.ai>
2025-02-06 20:20:27 +0000

    New commit

--- src/components/blocks/service-selector.tsx
+++ src/components/blocks/service-selector.tsx
@@ -20,7 +20,14 @@ export function ServiceSelector({ onSelect }: ServiceSelectorProps) {
         <SelectItem value="medicina-general-equina">Medicina General Equina - 80€</SelectItem>
         <SelectItem value="medicina-esportiva-equina">Medicina Esportiva Equina - 120€</SelectItem>
         <SelectItem value="odontologia-equina">Odontologia Equina - 90€</SelectItem>
+        <SelectItem value="cirurgia-equina">Cirurgia Equina - 500€</SelectItem>
+        <SelectItem value="reproduccio-equina">Reproducció Equina - 150€</SelectItem>
+        <SelectItem value="rehabilitacio-equina">Rehabilitació Equina - 100€</SelectItem>
+        <SelectItem value="medicina-preventiva-equina">Medicina Preventiva Equina - 70€</SelectItem>
         <SelectItem value="medicina-general">Medicina General - 50€</SelectItem>
+        <SelectItem value="cirurgia-general">Cirurgia General - 200€</SelectItem>
+        <SelectItem value="odontologia">Odontologia - 80€</SelectItem>
+        <SelectItem value="medicina-preventiva">Medicina Preventiva - 45€</SelectItem>
         <SelectItem value="urgencies">Urgències - 120€</SelectItem>
       </SelectContent>
     </Select>
